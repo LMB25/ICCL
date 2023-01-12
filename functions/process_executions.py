a31ded7f12f85db2c04e96df337af5bbd0faafc3
linabanana123 <91794325+linabanana123@users.noreply.github.com>
2023-01-12 18:03:27 +0100

    replaced process execution get graph method with ocpa method

--- functions/process_executions.py
+++ functions/process_executions.py
@@ -14,9 +14,14 @@ def get_events_process_exection(ocel, id):
 def get_objects_process_execution(ocel, id):
     return ocel.process_execution_objects[id]
 
+'''
 def get_process_execution_graph(ocel, id):
     ocel_process_executions = get_process_executions(ocel)
     return ocel.graph.eog.subgraph(ocel_process_executions[id])
+'''
+def get_process_execution_graph(ocel, id):
+    graph = ocel.get_process_execution_graph(id)
+    return graph
 
 def get_variant_process_executions(ocel):
     return ocel.variants_dict()
